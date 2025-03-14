4655e5f94d0acf60111597fea319f0175c7fd2e3
Prajwal Gurnule <iamprajwalgurnule@gmail.com>
2025-03-14 20:24:57 +0530

    Update commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './store';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import FormBuilder from './components/FormBuilder';
 import FormFiller from './components/FormFiller';
-import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/builder" element={<FormBuilder />} />
-            <Route path="/form/:formId" element={<FormFiller />} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/builder" element={<FormBuilder />} />
+          <Route path="/form/:formId" element={<FormFiller />} />
+        </Routes>
       </PersistGate>
     </Provider>
   );
